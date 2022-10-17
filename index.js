8883e512613806a83b818cbe9a578a087b0c2c91
Ahmed Tijani Akinfalabi <akinfalabiahmed@gmail.com>
2022-10-17 13:55:30 +0200

    completed

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
-const btn = document.getElementById("btn")
-const color = document.querySelector(".color")
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+function getRandom() {
+    return Math.floor(Math.random() * colors.length)
+}
 
 btn.addEventListener("click", function() {
-    const random = 2
+    const random = getRandom()
+    
     document.body.style.backgroundColor = colors[random]
     color.textContent = colors[random]
 })
